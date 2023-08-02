6b2db415aa21ee8a0f72f021566f0806430cf139
bradley-cantrell <cantrellbldev@gmail.com>
2023-08-02 19:55:29 -0400

    optimizations, setting variables and improving ux

--- main.go
+++ main.go
@@ -1,29 +1,40 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+
+	"github.com/eiannone/keyboard"
 )
 
-func main() {
-	fmt.Println(
-		"Welcome to Go Choose Your Own Adventure. Press Enter to begin the story:",
-	)
-	bufio.NewReader(os.Stdin).ReadBytes('\n')
-	
-	fmt.Println("Once upon a time there was a girl. She wanted to play a game. She could play Tic-Tac-Toe or Checkers. Which game should she play?.")
-	fmt.Println("Type 1 or 2 and then press Enter:\n 1. Tic-Tac-Toe\n 2. Checkers") 
+var greeting = "Welcome to Go Choose Your Own Adventure. Press any key to begin the story:"
+var storyIntro = "Once upon a time there was a girl. She wanted to play a game."
+var choiceOne = "checkers"
+var choiceTwo = "chess"
 
-	reader := bufio.NewReader(os.Stdin)
-	choice, _ := reader.ReadString('\n')
+func main() {
+	fmt.Println(greeting)
+	_, _, _ = keyboard.GetSingleKey()
+
+	fmt.Println(storyIntro)
+	fmt.Printf("She could play %s or she could play %s.\n", choiceOne, choiceTwo)
+	fmt.Println("Which game should she play?\n Select option 1 or 2:")
+	fmt.Printf("1. %s\n2. %s\n", choiceOne, choiceTwo)
+
+	char, _, err := keyboard.GetSingleKey()
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+	playStory(string(char))
+}
+
+func playStory(choice string) {
 	switch choice {
-	case "1\n":
-		fmt.Println("The girl plays Tic-Tac-Toe. She has fun.")
-	case "2\n":
-		fmt.Println("The girl plays Checkers. She has fun.")
+	case "1":
+		fmt.Printf("The girl plays %s. She has fun.\n", choiceOne)
+	case "2":
+		fmt.Printf("The girl plays %s. She has fun.\n", choiceTwo)
 	default:
 		fmt.Println("That wasn't one of her options.")
 	}
-
 }
